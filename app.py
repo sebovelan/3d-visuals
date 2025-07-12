7905d03c175c7db6f6f48b168f10795bd091209f
sebovelan <sebovelar@gmail.com>
2025-07-12 04:00:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -23,4 +23,4 @@ def upload_audio():
     return jsonify(result)
 
 if __name__ == "__main__":
-    app.run(debug=False)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
